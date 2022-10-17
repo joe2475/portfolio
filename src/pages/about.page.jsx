7818994adde84c9c99c35a22fdc82d286dc8dc20
Joseph Barsis <33675219+joe2475@users.noreply.github.com>
2022-10-17 13:52:56 -0500

    Update about.page.jsx
    Fixed minor grammar issue.

--- src/pages/about.page.jsx
+++ src/pages/about.page.jsx
@@ -9,7 +9,7 @@ const About = () =>
       <div>
             <Container>
                 <h1>About</h1>
-            <p>I am a former Computer Science and Software Engineering student, with a domain application of information assurance, at the University of Texas at Dallas. I work for Fidelity Investments as a associate software engineer. I go back for my masters degree here in the spring. Afterwards I plan on obtaining my doctorate degree. </p>
+            <p>I am a former Computer Science and Software Engineering student, with a domain application of information assurance, at the University of Texas at Dallas. I work for Fidelity Investments as an associate software engineer. I go back for my masters degree here in the spring. Afterwards I plan on obtaining my doctorate degree. </p>
                 </Container>
         </div>
     )
